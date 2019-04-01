236446d5cc127d8b6865d0c461915aa368bb8655
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-04-01 03:08:38 -0300

    Melhorando select

--- SCRIPTS/sistema-escola/SQL/SELECT.sql
+++ SCRIPTS/sistema-escola/SQL/SELECT.sql
@@ -2,14 +2,17 @@ select a.nm_nome
 	  , a.id_aluno
 	  , a.tx_email
 	  , a.serie
-	  , group_concat(m.nm_nome order by m.nm_nome separator ', ' ) as materias
+	  , group_concat(n.nr_nota  separator ', ' ) as notas
 from aluno a
 	, materia_aluno ma
-	, materia m
+	, materia	 	m
+	, nota 		 	n
 where ma.id_aluno = a.id_aluno
 and   ma.id_materia = m.id_materia
+and   ma.id_materia_aluno = n.id_materia_aluno
 group by a.id_aluno 
     
  
  
+
 
