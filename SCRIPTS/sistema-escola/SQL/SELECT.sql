50c4f1bb0132267b8bed5edf8fde2981bdf44c62
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-04-01 03:27:42 -0300

    Otimizando select

--- SCRIPTS/sistema-escola/SQL/SELECT.sql
+++ SCRIPTS/sistema-escola/SQL/SELECT.sql
@@ -2,15 +2,33 @@ select a.nm_nome
 	  , a.id_aluno
 	  , a.tx_email
 	  , a.serie
-	  , group_concat(n.nr_nota  separator ', ' ) as notas
+	  , (
+	  	select group_concat(n.nr_nota  order by n.nr_nota separator ', ' ) 	  	
+	  	from nota n
+	  		, materia_aluno ma
+	  	where n.id_materia_aluno = ma.id_materia_aluno
+	  	and   ma.id_aluno = a.id_aluno
+	  	and   ma.id_materia =  1
+	  	) as matematica
+	  , (
+	  	select group_concat(n.nr_nota  order by n.nr_nota separator ', ' ) 	  	
+	  	from nota n
+	  		, materia_aluno ma
+	  	where n.id_materia_aluno = ma.id_materia_aluno
+	  	and   ma.id_aluno = a.id_aluno
+	  	and   ma.id_materia =  2
+	  	) as portugues
+	  , (
+	  	select group_concat(n.nr_nota  order by n.nr_nota separator ', ' ) 	  	
+	  	from nota n
+	  		, materia_aluno ma
+	  	where n.id_materia_aluno = ma.id_materia_aluno
+	  	and   ma.id_aluno = a.id_aluno
+	  	and   ma.id_materia =  3
+	  	) as portugues
 from aluno a
-	, materia_aluno ma
-	, materia	 	m
-	, nota 		 	n
-where ma.id_aluno = a.id_aluno
-and   ma.id_materia = m.id_materia
-and   ma.id_materia_aluno = n.id_materia_aluno
-group by a.id_aluno 
+
+
     
  
  
